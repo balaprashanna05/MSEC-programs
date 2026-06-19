1846a149c10f5d9c57be20ea6008c1f334c76de4
Balaprashanna VB <balaprashannavmj2534@gmail.com>
2026-06-19 12:13:55 +0530

    Update pascal.java

--- Java/pascal.java
+++ Java/pascal.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class PascalsTriangle {
     public static void main(String[] args) {
-        int rows = 6; // Change this value to print more rows
+        int rows = 6; 
         printTriangle(rows);
     }
 
     public static void printTriangle(int numRows) {
         for (int i = 0; i < numRows; i++) {
-            // Print formatting spaces for the pyramid shape
             for (int space = 0; space < numRows - i; space++) {
                 System.out.print("  ");
             }
@@ -16,7 +15,6 @@ public class PascalsTriangle {
             int number = 1;
             for (int j = 0; j <= i; j++) {
                 System.out.printf("%4d", number);
-                // Calculate the next number using the current row ratio
                 number = number * (i - j) / (j + 1);
             }
             System.out.println();
